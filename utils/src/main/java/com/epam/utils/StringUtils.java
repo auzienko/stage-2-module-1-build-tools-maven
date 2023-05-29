490ea7eef38f492c1f2048f5b95495bb7aa6d493
Aleksandr Uzienko <a.uzienko@gmail.com>
2023-05-29 16:57:13 +0300

    solution

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,14 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         //code!
+        try {
+            return NumberUtils.createNumber(str).doubleValue() > 0;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
